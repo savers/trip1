48810e689436698c90a8cafcfa2c5933d36b44c4
SAvers <averss@gmail.com>
2015-09-26 12:29:18 +0300

    Настройки

--- config/web.php
+++ config/web.php
@@ -38,6 +38,11 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
+        'urlManager'=>[
+            'enablePrettyUrl'=>'true',
+            'showScriptName'=>false,
+
+        ],
     ],
     'params' => $params,
 ];
